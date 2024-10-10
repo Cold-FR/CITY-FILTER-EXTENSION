5fce34797d7e2666a0d08d3f90a944c6526e980c
ColdFR <david.clement5106@gmail.com>
2024-10-10 23:12:16 +0200

    Docs + words filter

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,8 +1,8 @@
 /**
  * @file This file contains the content script for the extension.
  * @author Cold-FR
- * @last-modified 2024-08-20
- * @version 1.0
+ * @last-modified 2024-10-10
+ * @version 2.0
  * @github https://github.com/Cold-FR/CITY-FILTER-EXTENSION
  */
 
@@ -11,7 +11,7 @@
  * @constant {Object}
  * @property {boolean} childList - Whether to observe the addition of new child nodes or removal of existing child nodes.
  * @property {boolean} subtree - Whether to extend monitoring to the entire subtree of nodes rooted at the target.
- * @default
+ * @default {childList: true, subtree: true}
  */
 const observerOptions = {childList: true, subtree: true};
 
@@ -19,7 +19,7 @@ const observerOptions = {childList: true, subtree: true};
  * The state of the chat observer.
  * True if the chat observer is active, false otherwise.
  * @type {boolean}
- * @default
+ * @default false
  */
 let chatObserverState = false;
 
@@ -27,34 +27,13 @@ let chatObserverState = false;
  * The chat observer.
  * Calls the filterMessages function when a new message is added to the chat.
  * @const {MutationObserver}
- * @default
+ * @default {new MutationObserver}
  */
 const chatObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (mutation.type === 'childList' && mutation.addedNodes.length > 0) filterMessages();
     });
 });
-
-/**
- * @type {string[]} - The list of usernames to filter.
- * @default
- */
-let usernamesFiltered = [];
-fetchUsernames();
-
-/**
- * Filters messages from the chat in game based on the global list of usernames to filter.
- * @function
- * @returns {void}
- */
-function filterMessages() {
-    const chats = Array.from(document.querySelectorAll('.bubble-container:not(.fetched)'));
-    chats.forEach((div) => {
-        div.classList.add('fetched');
-        const username = div.querySelector('.chat-content .username').innerText.replace(':', '').replace(' ', '');
-        if (usernamesFiltered.includes(username.toLowerCase())) div.style.display = 'none';
-    });
-}
 
 /**
  * Sets up the chat observer to active mode.
@@ -79,6 +58,63 @@ function disconnectChatObserver() {
 }
 
 /**
+ * The body observer.
+ * Check if the chat is added to the DOM and calls the setUpChatObserver function.
+ * @constant {MutationObserver}
+ * @default {new MutationObserver}
+ */
+const bodyObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+            if (mutation.addedNodes.length > 0 && !chatObserverState) {
+                if (mutation.addedNodes[0].classList && mutation.addedNodes[0].classList.contains('nitro-chat-widget')) setUpChatObserver();
+            } else if (mutation.removedNodes.length > 0 && chatObserverState) {
+                if (mutation.removedNodes[0].classList && mutation.removedNodes[0].classList.contains('nitro-chat-widget')) disconnectChatObserver();
+            }
+        }
+    });
+});
+
+bodyObserver.observe(document.getElementById('root'), observerOptions);
+
+/**
+ * @type {string[]} - The list of usernames to filter.
+ * @default []
+ */
+let usernamesFiltered = [];
+
+/**
+ * @type {string[]} - The list of words to filter.
+ * @default []
+ */
+let wordsFiltered = [];
+
+fetchUsernames();
+fetchWords();
+
+/**
+ * Filters messages from the chat in game based on the global list of usernames to filter.
+ * @function
+ * @returns {void}
+ */
+function filterMessages() {
+    const chats = Array.from(document.querySelectorAll('.bubble-container:not(.fetched)'));
+    chats.forEach((div) => {
+        div.classList.add('fetched');
+        const username = div.querySelector('.chat-content .username').innerText.replace(':', '').replace(' ', '');
+        if (usernamesFiltered.includes(username.toLowerCase())) div.style.display = 'none';
+
+        const msg = div.querySelector('.chat-content .message');
+        const msgContent = msg.innerText.toLowerCase();
+        if (wordsFiltered.some((word) => msgContent.includes(word))) {
+            msg.style.fontStyle = 'italic';
+            msg.style.color = '#595959';
+            msg.innerText = 'Message ignoré.';
+        }
+    });
+}
+
+/**
  * Fetches the list of usernames to filter from the storage.
  * @function
  * @returns {void}
@@ -92,25 +128,27 @@ function fetchUsernames() {
 }
 
 /**
- * The body observer.
- * Check if the chat is added to the DOM and calls the setUpChatObserver function.
- * @constant {MutationObserver}
- * @default
+ * Fetches the list of words to filter from the storage.
+ * @function
+ * @returns {void}
  */
-const bodyObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.type === 'childList') {
-            if (mutation.addedNodes.length > 0 && !chatObserverState) {
-                if (mutation.addedNodes[0].classList.contains('nitro-chat-widget')) setUpChatObserver();
-            } else if (mutation.removedNodes.length > 0 && chatObserverState) {
-                if (mutation.removedNodes[0].classList.contains('nitro-chat-widget')) disconnectChatObserver();
-            }
-        }
+function fetchWords() {
+    chrome.storage.local.get('words', (data) => {
+        if (!data.words || (data.words.length === 0 && wordsFiltered.length === 0)) return;
+
+        wordsFiltered = data.words.map((word) => word.toLowerCase());
     });
-});
+}
 
-bodyObserver.observe(document.getElementById('root'), observerOptions);
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+/**
+ * Listens for messages from the popup script.
+ * @function
+ * @param {Object} request - The request object.
+ * @returns {void}
+ * @listens chrome.runtime.onMessage
+ */
+chrome.runtime.onMessage.addListener((request) => {
     if (request.checkUsernames) fetchUsernames();
+
+    if (request.checkWords) fetchWords();
 });
