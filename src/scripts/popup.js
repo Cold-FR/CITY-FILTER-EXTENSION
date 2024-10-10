172a1d2970a6393776a15c6bb50320d05f6a0581
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2024-10-10 23:12:01 +0200

    Docs + change mode handle

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,22 +1,135 @@
-const ignoredPlayersList = document.getElementById('ignored-players-list');
+/**
+ * @file This file contains the popup script.
+ * @author Cold-FR
+ * @last-modified 2024-10-10
+ * @version 2.0
+ * @github https://github.com/Cold-FR/CITY-FILTER-EXTENSION
+ */
+
+/**
+ * The mode of the ignored values.
+ * @type {string}
+ * @default 'usernames'
+ */
+let mode = 'usernames';
+
+/**
+ * @type {HTMLDivElement} - The ignored values list.
+ * @default document.getElementById('ignored-values-list')
+ * @readonly
+ */
+const ignoredValuesList = document.getElementById('ignored-values-list');
+
+/**
+ * @type {HTMLDivElement} - The loader.
+ * @default document.getElementById('loader')
+ * @readonly
+ */
 const loader = document.getElementById('loader');
-document.getElementById('ignore-player').addEventListener('submit', (e) => {
+
+/**
+ * @type {HTMLFormElement} - The ignore add form.
+ * @default document.getElementById('ignore-add-form')
+ * @readonly
+ */
+const form = document.getElementById('ignore-add-form');
+
+/**
+ * The form submit event.
+ * @function
+ * @param {Event} e - The event.
+ * @returns {void}
+ * @listens submit
+ */
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    addIgnoredPlayer(e.target[0].value);
+    return addIgnoredValue(e.target[0].value);
 });
 
+/**
+ * The document ready event.
+ * @function
+ * @returns {void}
+ * @listens DOMContentLoaded
+ */
 document.addEventListener('DOMContentLoaded', () => {
-    displayIgnoredPlayers();
+    displayIgnoredValues();
     
     chrome.storage.local.get('darkMode', (data) => {
         if(data.darkMode) document.documentElement.classList.add('black');
     });
 });
 
+/**
+ * The color mode button click event.
+ * @function
+ * @returns {void}
+ * @listens click
+ */
 document.getElementById('colorMode').addEventListener('click', () => {
     toggleColorMode();
 });
+
+/**
+ * The change mode button.
+ * @type {HTMLDivElement}
+ * @default document.getElementById('changeMode')
+ */
+const btnChangeMode = document.getElementById('changeMode');
+
+/**
+ * The change mode button click event.
+ * @function
+ * @returns {void}
+ * @listens click
+ */
+btnChangeMode.addEventListener('click', () => {
+    showLoader();
+
+    btnChangeMode.classList.toggle('usernames');
+    if(mode === 'usernames') {
+        mode = 'words';
+    } else {
+        mode = 'usernames';
+    }
+
+    document.getElementById('head-title').innerText = mode === 'usernames' ? 'Ignorer un joueur' : 'Ignorer un mot';
+    if(mode === 'usernames') {
+        document.getElementById('head-description').innerText ='Vous pouvez ignorer des joueurs pour ne plus voir leurs messages apparaître dans le tchat.'
+    } else {
+        document.getElementById('head-description').innerText = 'Vous pouvez ignorer des mots pour ne plus voir les messages contenant ces mots apparaître dans le tchat.';
+    }
+    form.dataset.mode = mode;
+    document.getElementById('ignore-input').placeholder = mode === 'usernames' ? 'Nom d\'utilisateur' : 'Mot à ignorer';
+    document.getElementById('ignore-submit').value = mode === 'usernames' ? 'Ignorer le joueur' : 'Ignorer le mot';
+    document.getElementById('ignored-values-title').innerText = `${mode === 'usernames' ? 'Joueurs' : 'Mots'} ignorés`;
+    document.getElementById('ignored-values-description').innerText = `Vous pouvez retirer un ${mode === 'usernames' ? 'joueur' : 'mot'} de la liste en cliquant dessus.`;
+
+    displayIgnoredValues();
+
+    hideLoader();
+});
+
+/**
+ * Shows the loader.
+ * @function
+ * @returns {void}
+ */
+function showLoader() {
+    loader.style.display = 'flex';
+}
+
+/**
+ * Hides the loader.
+ * @function
+ * @returns {void}
+ */
+function hideLoader() {
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 300);
+}
 
 /**
  * Toggles the color mode.
@@ -25,80 +138,79 @@ document.getElementById('colorMode').addEventListener('click', () => {
  */
 function toggleColorMode() {
     if(document.documentElement.classList.contains('black')) {
-        document.documentElement.classList.remove('black');
-        chrome.storage.local.set({ darkMode: false });
+        chrome.storage.local.set({darkMode: false}).then(r => document.documentElement.classList.remove('black'));
     } else {
-        document.documentElement.classList.add('black');
-        chrome.storage.local.set({ darkMode: true });
+        chrome.storage.local.set({darkMode: true}).then(r => document.documentElement.classList.add('black'));
     }
 }
 
 /**
- * Adds a username to the ignored players list.
+ * Adds a value to the ignored list.
  * @function
- * @param {string} username - The username to add.
+ * @param {string} value - The value to add.
  * @returns {void}
  */
-function addIgnoredPlayer(username) {
-    chrome.storage.local.get('usernames', (data) => {
-        const usernames = data.usernames || [];
-        const usernamesCheck = usernames.map((username) => username.toLowerCase());
-        if(usernamesCheck.includes(username.toLowerCase())) return;
-        usernames.push(username);
+function addIgnoredValue(value) {
+    chrome.storage.local.get(mode, (data) => {
+        const values = data[mode] || [];
+        const valuesCheck = values.map((value) => value.toLowerCase());
+        if(valuesCheck.includes(value.toLowerCase())) return;
+        values.push(value);
 
-        chrome.storage.local.set({ usernames: usernames }, () => {
-            displayIgnoredPlayers();
-            sendCheckUsernames();
+        chrome.storage.local.set({ [mode]: values }, () => {
+            displayIgnoredValues();
+            sendCheckValues();
         });
     });
 }
 
 /**
- * Displays the ignored players list.
+ * Displays the ignored values list.
  * @function
  * @returns {void}
  */
-function displayIgnoredPlayers() {
-    ignoredPlayersList.innerHTML = '';
+function displayIgnoredValues() {
+    ignoredValuesList.innerHTML = '';
 
-    chrome.storage.local.get('usernames', (data) => {
-        if (data.usernames && data.usernames.length > 0) {
-            data.usernames.forEach((username) => {
-                const newUsernames = data.usernames.filter((u) => u !== username);
+    chrome.storage.local.get(mode, (data) => {
+        if (data[mode] && data[mode].length > 0) {
+            data[mode].forEach((value) => {
                 const div = document.createElement('div');
-                div.classList.add('ignored-player');
-                div.textContent = username;
-                div.addEventListener('click', () => {
-                    loader.style.display = 'flex';
-                    chrome.storage.local.set({ usernames: newUsernames }, () => {
-                        sendCheckUsernames();
-                        newUsernames.length === 0 ? displayIgnoredPlayers() : div.remove();
-                        setTimeout(() => {
-                            loader.style.display = 'none';
-                        }, 300);
+                div.classList.add('ignored-value');
+                div.innerText = value;
+
+                div.addEventListener('click', async () => {
+                    const currentValues = await chrome.storage.local.get(mode);
+                    const newValues = currentValues[mode].filter((v) => v !== value);
+                    showLoader();
+                    chrome.storage.local.set({ [mode]: newValues }, () => {
+                        sendCheckValues();
+                        newValues.length === 0 ? displayIgnoredValues() : div.remove();
+                        hideLoader();
                     });
                 });
-                ignoredPlayersList.appendChild(div);
+
+                ignoredValuesList.appendChild(div);
             });
         } else {
             const p = document.createElement('p');
             p.classList.add('empty');
-            p.textContent = 'Vous n\'avez ignoré aucun joueur.';
-            ignoredPlayersList.appendChild(p);
+            p.innerText = `Vous n'avez ignoré aucun ${mode === 'usernames' ? 'joueur' : 'mot'}.`;
+            ignoredValuesList.appendChild(p);
         }
     });
 }
 
 /**
- * Sends a message to the content script to check the usernames.
+ * Sends a message to the content script to check the values.
  * @function
  * @returns {void}
  */
-function sendCheckUsernames() {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+function sendCheckValues() {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         const activeTab = tabs[0];
-        if(!activeTab.url.match('habbocity.me')) return;
+        if (!activeTab.url.match('habbocity.me')) return;
 
-        chrome.tabs.sendMessage(activeTab.id, { checkUsernames: true });
+        chrome.tabs.sendMessage(activeTab.id, {checkUsernames: mode === 'usernames', checkWords: mode === 'words'});
     });
 }
